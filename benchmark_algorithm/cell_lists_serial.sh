73f52d5502009dbc5a48675cf0a07d6462470597
Jaan Tollander de Balsch <jaan@hey.com>
2021-04-04 12:49:41 +0300

    Added extension

--- benchmark_algorithm/cell_lists_serial.sh
+++ benchmark_algorithm/cell_lists_serial.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=1000
-#SBATCH --output=output/cell_lists_serial_%j
+#SBATCH --output=output/cell_lists_serial_%j.out
 TIMESTAMP=$(date -Iseconds)
 module load julia
 srun julia run.jl --ns "[1, 5, 10:10:100...]" -d 2 -r 0.01 -i 1 --dir "output/cell_lists_serial_$TIMESTAMP" --algorithm "cell_lists"
