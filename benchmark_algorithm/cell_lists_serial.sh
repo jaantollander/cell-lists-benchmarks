08ad81584d99d51e229810e1b7cd95b09fc741d7
Jaan Tollander de Balsch <jaan@hey.com>
2021-04-04 20:31:09 +0300

    Increased iterations

--- benchmark_algorithm/cell_lists_serial.sh
+++ benchmark_algorithm/cell_lists_serial.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=output/cell_lists_serial_%j.out
 TIMESTAMP=$(date -Iseconds)
 module load julia
-srun julia run.jl --ns "[1, 5, 10:10:100...]" -d 2 -r 0.01 -i 1 --dir "output/cell_lists_serial_$TIMESTAMP" --algorithm "cell_lists"
-srun julia run.jl --ns "[1, 5, 10:10:100...]" -d 3 -r 0.01 -i 1 --dir "output/cell_lists_serial_$TIMESTAMP" --algorithm "cell_lists"
-srun julia run.jl --ns "[1, 5, 10:10:200...]" -d 4 -r 0.01 -i 1 --dir "output/cell_lists_serial_$TIMESTAMP" --algorithm "cell_lists"
-srun julia run.jl --ns "[1, 5, 10:10:300...]" -d 5 -r 0.01 -i 1 --dir "output/cell_lists_serial_$TIMESTAMP" --algorithm "cell_lists"
+srun julia run.jl --ns "[1, 5, 10:10:100...]" -d 2 -r 0.01 -i 10 --dir "output/cell_lists_serial_$TIMESTAMP" --algorithm "cell_lists"
+srun julia run.jl --ns "[1, 5, 10:10:100...]" -d 3 -r 0.01 -i 10 --dir "output/cell_lists_serial_$TIMESTAMP" --algorithm "cell_lists"
+srun julia run.jl --ns "[1, 5, 10:10:200...]" -d 4 -r 0.01 -i 10 --dir "output/cell_lists_serial_$TIMESTAMP" --algorithm "cell_lists"
+srun julia run.jl --ns "[1, 5, 10:10:300...]" -d 5 -r 0.01 -i 10 --dir "output/cell_lists_serial_$TIMESTAMP" --algorithm "cell_lists"
