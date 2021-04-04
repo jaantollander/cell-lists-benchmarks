f26b9b72ca523de1781e10074df05c36c06a6eea
Jaan Tollander de Balsch <jaan@hey.com>
2021-04-04 12:47:58 +0300

    Fixed typo

--- benchmark_algorithm/cell_list_constructor_serial.sh
+++ benchmark_algorithm/cell_list_constructor_serial.sh
@@ -2,6 +2,6 @@
 #SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=1000
-#SBATCH --ouput=output/cell_list_constructor_serial_%j
+#SBATCH --output=output/cell_list_constructor_serial_%j
 module load julia
 srun julia run.jl --ns "[1, 10, 25, 35, 50, 75, 100:50:1000...]" -d 2 -r 0.1 -i 1 --dir "output/cell_list_constructor_serial_$(date -Iseconds)" --algorithm "cell_list_serial"
